88dcb6d13d5a1408cb85ff103b55b21182dfb25a
Prajwal Kamble <prajwal17022000@gmail.com>
2026-05-28 19:58:48 +0530

    Path parameters and query parameters

--- main.py
+++ main.py
@@ -36,3 +36,22 @@ def view_patient(patient_id:str= Path(...,description="Id of patient in data",ex
     if patient_id in data:
         return data[patient_id]
     raise HTTPException(status_code=404,detail="Patient not found")
+
+# end point for query parameter
+@app.get("/sort")
+def sort_patients(sort_by:str=Query(...,description="Sort on the basis of height,weight or bmi"),order:str= Query("asc",description="sort in ascending or descending order")):
+    valid_field = ["height","weight","bmi"]
+    if sort_by not in valid_field:
+        raise HTTPException(status_code=400,detail=f"Invalid Field select from {valid_field}")
+    if order not in ["asc","desc"]:
+        raise HTTPException(status_code=400,detail=f"Invalid order select from {["asc","desc"]}")
+    
+    data = load_data()
+
+    sort_order = True if order == "desc" else False
+
+    sorted_data = sorted(data.values(),key=lambda x: x.get(sort_by,0),reverse=sort_order)
+
+    return sorted_data
+
+    
